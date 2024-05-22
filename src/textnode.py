c1a1a1b8847628e81d1319ff8177fa92e0cd2afc
ExgleGaming <EaglesFan91x@outlook.com>
2024-05-21 23:41:36 -0400

    TextNode class created & __eq__ method created

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,9 @@
-class TextNode(self.text, self.text_type, self.url):
+class TextNode(self, text, text_type, url):
     def __init__():
-        pass
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        if self.text == self.text_type:
+            return True
