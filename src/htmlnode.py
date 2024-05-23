2107e4fb4cb28bf4b4e45b73ef88d083645addb8
ExgleGaming <EaglesFan91x@outlook.com>
2024-05-22 20:59:59 -0400

    Basic nodes functionality complete

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,3 +18,38 @@ class HTMLNode:
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("Invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        props_html = self.props_to_html()
+        return f"<{self.tag}{props_html}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        
+        super().__init__(tag, None, children, props)
+    
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("Invalid HTML: no children")
+
+        html_children = ""
+        for child in self.children:
+            html_children += child.to_html()
+
+        return f"<{self.tag}>{html_children}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
